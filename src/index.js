2caf2fa26a8c7bc302dc212ca0a55823e10e0c2c
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-09-02 17:51:43 +0530

    Add basic game loop

--- src/index.js
+++ src/index.js
@@ -1,18 +1,41 @@
-import Ship from './modules/Ship';
 import GameBoard from './modules/GameBoard';
+import Player from './modules/Player';
 
-let board = GameBoard();
-console.log(board);
+const player1 = Player();
+const computer = Player();
 
-board.placeShip([1,2]);
-console.log(board);
+const  playerBoard = GameBoard();
+const computerBoard = GameBoard();
 
-board.receiveAttack(1);
-board.receiveAttack(0);
-board.receiveAttack(2);
+console.log("player 1, place your ships")
+playerBoard.placeShip([1, 2, 3]);
+playerBoard.placeShip([10, 20, 30]);
+playerBoard.placeShip([50]);
 
-board.placeShip([3]);
+computerPlacement();
 
-console.log(board);
-console.log(board.ships[0].isSunk());
-console.log(board.allShipsSunk());
+function computerPlacement() {
+    computerBoard.placeShip([1]);
+    computerBoard.placeShip([10, 11]);
+    computerBoard.placeShip([90, 91, 92]);
+}
+
+console.log(playerBoard);
+console.log(computerBoard);
+
+console.log("placement is done, let the game begin")
+
+let turn = "player1";
+
+while(!playerBoard.allShipsSunk() && !computerBoard.allShipsSunk()) {
+    if(turn === "player1") {
+        const position = prompt("enter position to attack");
+        player1.attack(computerBoard, position);
+    } else {
+        const position = Math.round(Math.random() * 100);
+        console.log(position);
+    }
+    turn = turn === "player1" ? "computer" : "player1";
+    console.log(playerBoard.allShipsSunk());
+    console.log(computerBoard.allShipsSunk());
+}
