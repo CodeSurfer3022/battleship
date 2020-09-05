ebe02d7f256acf622c1a6ac12d275cfb489d02ca
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-09-05 15:13:50 +0530

    update id of container and remove seperate module for actions

--- src/modules/BoardRender.js
+++ src/modules/BoardRender.js
@@ -1,7 +1,8 @@
-function renderBoard(container, board) {
+function renderBoard(container, board, id) {
 
     const displayBoard = document.createElement('div');
     displayBoard.classList.add('board');
+    displayBoard.id = id;
 
     board.forEach( (position, index = 0) => {
         const cell = document.createElement('div');
