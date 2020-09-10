c81938112195d351351585cecdbb12998d5ff1bd
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-09-10 16:18:21 +0530

    Revert "refactor code for placing ship divs"

--- src/modules/DomManipulation/ShipDivPlacement.js
+++ src/modules/DomManipulation/ShipDivPlacement.js
@@ -1,37 +1,42 @@
-function placeShipDivs(container, boxNum, orientation, length, start, end) {
+function makeShipDiv() {
     const div = document.createElement('div');
-    div.setAttribute('data-box', boxNum);
     div.classList.add('ship');
-    div.classList.add(orientation);
     div.draggable = true;
+    return div;
+}
 
-    div.setAttribute('data-length', length);
+function positionShip(ship, start) {
+    const topOffset = Math.floor(start/ 10);
+    const leftOffset = start % 10;
+
+    ship.style.top = `${topOffset * 42}px`;
+    ship.style.left = `${leftOffset * 42}px`;
+}
+
+function placeShipDivs(container, boxNum, orientation, length, start, end) {
+    const ship = makeShipDiv();
+
+    ship.classList.add(orientation);
+    ship.setAttribute('data-box', boxNum);
+    ship.setAttribute('data-length', length);
 
     let topOffset, leftOffset;
     if(orientation === 'vertical') {
-        div.setAttribute('data-top', start);
-        div.setAttribute('data-bottom', end);
+        ship.setAttribute('data-top', start);
+        ship.setAttribute('data-bottom', end);
 
-        div.style.width = '40px';
-        div.style.height = `${40 * length}px`;
-
-        topOffset = Math.floor(start/ 10);
-        leftOffset = start % 10;
+        ship.style.width = '40px';
+        ship.style.height = `${40 * length}px`;
     } else {
-        div.setAttribute('data-left', start);
-        div.setAttribute('data-right', end);
+        ship.setAttribute('data-left', start);
+        ship.setAttribute('data-right', end);
 
-        div.style.height = '40px';
-        div.style.width = `${40 * length}px`
-
-        topOffset = Math.floor(start/ 10);
-        leftOffset = start % 10;
+        ship.style.height = '40px';
+        ship.style.width = `${40 * length}px`
     }
-    console.log(div);
-    container.appendChild(div);
-
-    div.style.top = `${topOffset * 42}px`;
-    div.style.left = `${leftOffset * 42}px`;
+    positionShip(ship, start);
+    console.log(ship);
+    container.appendChild(ship);
 }
 
 export default placeShipDivs;
