757dbe1852ade6f74d542c81665262121b9e5d13
Chandra Prakash <pr.chandraprakash@gmail.com>
2021-04-28 21:41:36 +0530

    Page layout and basic styling for header and footer

--- battleship/src/App.js
+++ battleship/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import Main from "./Components/Main";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faHeart} from "@fortawesome/free-solid-svg-icons";
+// import {faLinkedinIn, faGithub} from "@fortawesome/free-brands-svg-icons";
+// import {faEnvelope, faPhoneAlt} from '@fortawesome/free-solid-svg-icons'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <h1>Battleship</h1>
       </header>
+      <Main />
+      <footer>
+        <p>Made by Chandra.</p>
+        <p>Hosted with <FontAwesomeIcon icon={faHeart} /> by github.</p>
+      </footer>
     </div>
   );
 }
