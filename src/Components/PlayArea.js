07272c3ab661683bd675c42c7585930e1d4a10e1
Chandra Prakash <pr.chandraprakash@gmail.com>
2021-04-30 23:50:47 +0530

    Use single variable to hold board values of both boards

--- src/Components/PlayArea.js
+++ src/Components/PlayArea.js
@@ -2,47 +2,32 @@ import React, {useState} from "react";
 import "./PlayArea.css";
 
 import GameBoardContainer from "./GameBoardContainer";
-import Board from "../Factories/Board";
-import Player from "../Factories/Player";
 import getComputerAttackPosition from "../Helpers/getComputerAttackPosition";
-import makeShips from "../Helpers/makeShips";
 
 // The board initially has no ships, so initialize to undefined
 let initialValues = [];
 for(let i = 0; i < 100; i ++) initialValues.push(undefined);
 
-// create Players to access attack functionality
-let player = Player('Player');
-let computer = Player('Computer');
-
-// make boards for players
-let playerBoard = Board();
-let computerBoard = Board();
-
-// make ships for players
-let playerShips = makeShips();
-let computerShips = makeShips();
-
-// place ships on the respective player's board
-playerBoard.placeAllShips(playerShips);
-computerBoard.placeAllShips(computerShips);
-
-function PlayArea() {
-  const [playerBoardValues, setPlayerBoardValues] = useState(initialValues);
-  const [computerBoardValues, setComputerBoardValues] = useState(initialValues);
+function PlayArea(props) {
+  const {player, computer} = props;
+  const [boardValues, setBoardValues] = useState(
+    {
+      player: initialValues,
+      computer: initialValues
+    })
 
   return(
     <div className="play-area">
       <GameBoardContainer
         player={player.name}
-        board={playerBoardValues}
-        ships={playerShips}
+        board={boardValues.player}
+        ships={player.ships}
       />
 
       <GameBoardContainer
         player={computer.name}
-        board={computerBoardValues}
-        ships={computerShips}
+        board={boardValues.computer}
+        ships={computer.ships}
       />
     </div>
   )
