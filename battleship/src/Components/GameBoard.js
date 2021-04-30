74ca3f6e5481d815de37e2b74e0616047c8223c2
Chandra Prakash <pr.chandraprakash@gmail.com>
2021-04-30 11:16:40 +0530

    Add function to check if all ships are sunk

--- battleship/src/Components/GameBoard.js
+++ battleship/src/Components/GameBoard.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import "./GameBoard.css";
 import Cell from "./Cell";
 import makeShips from "../Helpers/makeShips";
@@ -19,19 +19,26 @@ function GameBoard(props) {
   function receiveAttack(position) {
     console.log(position, boardCellValues[position]);
     let newValues = boardCellValues.slice();
+    let newShips = ships.slice();
 
     let value = newValues[position];
     if(value === undefined) {
       newValues[position] = 'miss';
     } else if (newValues[position] !== 'miss' && newValues[position] !== 'hit'){
       const index = newValues[position];
-      const hitShip = ships[index];
+      const hitShip = newShips[index];
       console.log(hitShip);
       hitShip.hit(position);
       newValues[position] = 'hit';
     }
     console.log(newValues);
     setBoardCellValues(newValues);
+    setShips(newShips);
+    console.log(areAllShipsSunk());
+  }
+
+  function areAllShipsSunk() {
+    return ships.every(ship => ship.isSunk());
   }
 
 
