4fb1221f453c4433bdc0e11643a11d4e52ccda21
Chandra Prakash <pr.chandraprakash@gmail.com>
2021-04-30 14:38:09 +0530

    move state values to container so they can be accessed by shipsStatus

--- battleship/src/Components/GameBoard.js
+++ battleship/src/Components/GameBoard.js
@@ -1,61 +1,13 @@
-import React, {useState} from "react";
+import React from "react";
 import "./GameBoard.css";
 import Cell from "./Cell";
-import makeShips from "../Helpers/makeShips";
-import placeShipsOnBoard from "../Helpers/placeShipsOnBoard";
 import Ship from "./Ship";
 import getComputerAttackPosition from "../Helpers/getComputerAttackPosition";
-
-// make initial ships and place them on board
-let initialShips = makeShips();
-let initialBoardCellValues = placeShipsOnBoard(initialShips);
 
 function GameBoard(props) {
   console.log(props.player)
 
-  const [ships, setShips] = useState(initialShips);
-  const [boardCellValues, setBoardCellValues] = useState(initialBoardCellValues);
-
-  console.log(ships, boardCellValues);
-
-  function attack(position) {
-    console.log(position, boardCellValues[position]);
-    let newValues = boardCellValues.slice();
-    let newShips = ships.slice();
-
-    let value = newValues[position];
-    if(value === undefined) {
-      newValues[position] = 'miss';
-
-    } else if (newValues[position] !== 'miss' && newValues[position] !== 'hit'){
-      const index = newValues[position];
-      const hitShip = newShips[index];
-      console.log(hitShip);
-      hitShip.hit(position);
-      newValues[position] = 'hit';
-    }
-    console.log(newValues);
-    setBoardCellValues(newValues);
-    setShips(newShips);
-    console.log(areAllShipsSunk());
-  }
-
-  function playRound(event) {
-    let cell = event.target;
-    let position = cell.getAttribute('data-key');
-    // this is player's turn
-    attack(position);
-
-    // then computer's turn
-    // position = getComputerAttackPosition(boardCellValues);
-    // attack(position);
-  }
-
-  function areAllShipsSunk() {
-    return ships.every(ship => ship.isSunk());
-  }
-
-  const cells = boardCellValues.map((value, index = 0) =>
+  const cells = props.boardCellValues.map((value, index = 0) =>
     <Cell key={index}
           value={value}
           index={index}
@@ -63,14 +15,14 @@ function GameBoard(props) {
 
   // place ship divs on board only for player
   const shipDivs = props.player === 'Player' ?
-    ships.map((ship, index) => <Ship
+    props.ships.map((ship, index) => <Ship
       key={index}
       index={index}
       ship={ship}/>) : null;
 
   return(
     <div className={`game-board ${props.player}`}>
-      <div className="board" onClick={playRound}>
+      <div className="board" onClick={props.playRound}>
         {cells}
         {shipDivs}
       </div>
