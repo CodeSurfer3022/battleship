5417871fceac4c4b14f76883b2e04422e5843a66
Chandra Prakash <pr.chandraprakash@gmail.com>
2021-05-04 15:22:15 +0530

    Add more tests for ship

--- src/tests/Ship.test.js
+++ src/tests/Ship.test.js
@@ -1,5 +1,21 @@
 import Ship from "../Factories/Ship";
 
+let tmp = Ship([1,2], 'vertical');
+
 test('returns an object', () => {
-  expect(typeof(Ship([], {}))).toBe('object');
+  expect(typeof(tmp)).toBe('object');
+});
+
+test('accepts a hit', () => {
+  tmp.hit(1);
+  expect(tmp.getPositionsHealth()[1]).toBe(0);
+})
+
+test('ship is not sunk', () => {
+  expect(tmp.isSunk()).toBe(false);
+})
+
+test('ship is sunk', () => {
+  tmp.hit(2);
+  expect(tmp.isSunk()).toBe(true);
 })
