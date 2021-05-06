bba1370df9087bbdc4a6a16e4074369e99354e26
Chandra Prakash <pr.chandraprakash@gmail.com>
2021-05-07 00:46:45 +0530

    Get old end and positions and calculate new end and positions for ship div

--- src/Helpers/ShipDivMovementHandler.js
+++ src/Helpers/ShipDivMovementHandler.js
@@ -1,4 +1,4 @@
-// function moveShipDiv(ship, cell, orientation, length) {
+function moveShipDiv(ship, cell, orientation, length) {
 //     if(orientation === 'vertical') {
 //         const newBottom = cell.getAttribute('data-key');
 //         const newTop = newBottom - (length - 1) * 10;
@@ -13,28 +13,55 @@
 //
 //         styleShip(ship, 'horizontal', newLeft, newRight);
 //     }
-// }
-//
-// function getPositionsFromShipDiv(orientation, length, end) {
-//     const positions = [];
-//     let position = end;
-//
-//     const decrement = orientation === 'vertical' ? 10 : 1;
-//
-//     for(let i = 0; i < length; i ++) {
-//         positions.push(position);
-//         position -= decrement;
-//     }
-//     return positions;
-// }
-//
-// function areValidPositions(newPositions, oldPositions) {
+}
+
+function getPositionsFromShipDiv(orientation, length, end) {
+    const positions = [];
+    let position = end;
+
+    const decrement = orientation === 'vertical' ? 10 : 1;
+
+    for(let i = 0; i < length; i ++) {
+        positions.push(position);
+        position -= decrement;
+    }
+    return positions;
+}
+
+function areValidPositions(newPositions, oldPositions) {
 //     return newPositions.every(newPosition => {
 //         // each position must either be empty or one of previous positions
 //         return (selfBoard.boardArray[newPosition] === undefined
 //                 || oldPositions.includes(newPosition)
 //         )
 //     });
-// }
-//
-// export  {moveShipDiv, getPositionsFromShipDiv, areValidPositions};
+}
+
+function handleDrop(ship, cell) {
+  // move the div
+  const orientation = [...ship.classList].includes('horizontal') ? 'horizontal' : 'vertical';
+  const length = ship.getAttribute('data-length');
+
+  console.log(orientation,length);
+
+  // get old end and positions
+  const oldEnd = parseInt(ship.getAttribute('data-end'));
+  const oldPositions = getPositionsFromShipDiv(orientation, length, oldEnd);
+  console.log(oldEnd, oldPositions);
+
+  // check if moving this div will cause any of the positions to overlap
+  const newEnd = parseInt(cell.getAttribute('data-key'));
+  const newPositions = getPositionsFromShipDiv(orientation, length, newEnd);
+
+  if(!areValidPositions(newPositions, oldPositions)) return;
+  console.log(newEnd, newPositions);
+  //
+  // moveShipDiv(ship, cell, orientation, length);
+  //
+  // const shipIndex = parseInt(ship.getAttribute('data-ship'));
+  // console.log(shipIndex, oldPositions, newPositions);
+  // selfBoard.updateShip(shipIndex, oldPositions, newPositions);
+
+}
+
+export  {handleDrop, moveShipDiv, getPositionsFromShipDiv, areValidPositions};
