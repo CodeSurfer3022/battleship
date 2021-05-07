15bc4ed0fab4818394f542df45bead0dbe1a46be
Chandra Prakash <pr.chandraprakash@gmail.com>
2021-05-07 21:31:37 +0530

    validate and move shipDivs

--- src/Helpers/ShipDivMovementHandler.js
+++ src/Helpers/ShipDivMovementHandler.js
@@ -1,48 +1,52 @@
-function moveShipDiv(ship, cell, orientation, length) {
-//     if(orientation === 'vertical') {
-//         const newBottom = cell.getAttribute('data-key');
-//         const newTop = newBottom - (length - 1) * 10;
-//         if(newTop < 0) return;
-//
-//         styleShip(ship, 'vertical', newTop, newBottom);
-//     } else {
-//         const newRight = cell.getAttribute('data-key');
-//         const row = Math.floor(newRight / 10);
-//         const newLeft = newRight - (length - 1);
-//         if(newLeft < row * 10) return;
-//
-//         styleShip(ship, 'horizontal', newLeft, newRight);
-//     }
+import {getOffsets} from "./styleShip";
+
+function updateShipDivData(ship, start, end) {
+  ship.setAttribute('data-start', start);
+  ship.setAttribute('data-end', end);
 }
 
-function getPositionsFromShipDiv(orientation, length, end) {
-    const positions = [];
-    let position = end;
+function getPositionsFromShipDiv(orientation, length, start) {
+  const positions = [];
+  let position = start;
 
-    const decrement = orientation === 'vertical' ? 10 : 1;
+  const increment = orientation === 'vertical' ? 10 : 1;
 
-    for(let i = 0; i < length; i ++) {
-        positions.push(position);
-        position -= decrement;
-    }
-    return positions;
+  for (let i = 0; i < length; i++) {
+    positions.push(position);
+    position += increment;
+  }
+  return positions;
 }
 
-function areValidPositions(newPositions, oldPositions) {
-//     return newPositions.every(newPosition => {
-//         // each position must either be empty or one of previous positions
-//         return (selfBoard.boardArray[newPosition] === undefined
-//                 || oldPositions.includes(newPosition)
-//         )
-//     });
+function areValidPositions(playerBoard, newPositions, oldPositions, orientation, start, end) {
+  // each position must either be empty or one of previous positions
+  const areValidPositions = newPositions.every(newPosition =>
+    playerBoard[newPosition] === undefined
+    || oldPositions.includes(newPosition));
+
+  console.log(areValidPositions);
+
+  // and none of the new positions are out of the board
+  const arePositionsOnBoard = !newPositions.some(newPosition => newPosition > 99 || newPosition < 0);
+  console.log(start, end);
+  console.log(arePositionsOnBoard)
+
+  // all positions are on the same row for horizontal ships
+  // and same column for vertical ships
+  let inline =  orientation === 'horizontal' ? Math.floor(start/10) === Math.floor(end/10)
+    : start % 10 === end % 10;
+  console.log(inline);
+
+  return areValidPositions && arePositionsOnBoard && inline;
+
 }
 
-function handleDrop(ship, cell) {
+function handleDrop(player, ship, cell) {
   // move the div
   const orientation = [...ship.classList].includes('horizontal') ? 'horizontal' : 'vertical';
   const length = ship.getAttribute('data-length');
 
-  console.log(orientation,length);
+  console.log(orientation, length);
 
   // get old end and positions
   const oldEnd = parseInt(ship.getAttribute('data-end'));
@@ -50,18 +54,26 @@ function handleDrop(ship, cell) {
   console.log(oldEnd, oldPositions);
 
   // check if moving this div will cause any of the positions to overlap
-  const newEnd = parseInt(cell.getAttribute('data-key'));
-  const newPositions = getPositionsFromShipDiv(orientation, length, newEnd);
+  const newStart = parseInt(cell.getAttribute('data-key'));
+  const newPositions = getPositionsFromShipDiv(orientation, length, newStart);
 
-  if(!areValidPositions(newPositions, oldPositions)) return;
-  console.log(newEnd, newPositions);
-  //
-  // moveShipDiv(ship, cell, orientation, length);
-  //
-  // const shipIndex = parseInt(ship.getAttribute('data-ship'));
-  // console.log(shipIndex, oldPositions, newPositions);
-  // selfBoard.updateShip(shipIndex, oldPositions, newPositions);
+  if (!areValidPositions(player.board, newPositions, oldPositions, orientation,
+    newStart, newPositions[length - 1])) return;
+
+  console.log(newStart, newPositions);
+
+  // Now since the new positions are valid, we can move the ship
+  // div by styling it
+  updateShipDivData(ship, newPositions[0], newStart);
+  const {topOffset, leftOffset} = getOffsets(newPositions[0])
+  ship.style.top = `${topOffset}px`;
+  ship.style.left = `${leftOffset}px`;
+
+  const shipIndex = parseInt(ship.getAttribute('data-ship'));
+
+  // update ships info on player board
+  player.board.updateShip(shipIndex, oldPositions, newPositions);
 
 }
 
-export  {handleDrop, moveShipDiv, getPositionsFromShipDiv, areValidPositions};
+export default handleDrop;
